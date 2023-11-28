df6755f9062a54a300d3f4a9d521105df4df578c
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-28 10:05:26 -0800

    add files

--- quiz.js
+++ quiz.js
@@ -1,4 +1,19 @@
 const container = document.getElementById("container");
+const title = document.getElementById('title');
+
+const questionsAndAnswers = {
+    '1': ['In what year was the internet opened to the public?', '1993'],
+    '2': ['What is the meaning of "fn" on your computer keyboard?', 'function'],
+    '3': ['Who was the first computer programmer?', 'Ada Lovelace'],
+    '4': ['Amazon (company) was founded in which US state', 'Washington'],
+    '5': ['Which country has the largest population in the world?','India'],
+    '6': ['What capital has the longest name in the world?', 'Bangkok'],
+    '7': ['Which country has the largest desert?', 'Morocco'],
+    '8': ['What continent is the biggest?', 'Asia'],
+    '9': ['What is the best-selling video game ever as of 2023', 'Minecraft'],
+    '10': ['What is the capital of Vietnam?', 'Ha Noi']
+};
+
 document.getElementById("right").onclick = function() {
     document.getElementById("right").classList.add('correct');
     next();
@@ -6,21 +21,21 @@ document.getElementById("right").onclick = function() {
 function next(){
     let nextBtn = document.createElement("button");
     nextBtn.innerText = "Next ->";
-    // next.onclick = question();
-
+    nextBtn.onclick = showQuestion();
     container.appendChild(nextBtn);
 }
-// function question(){
-//     let html = `<section>
-//         <h1>What is the capital of Vietnam?</h1>
-//         <div>
-//             <button id = "right">Ha Noi</button>
-//             <button>Ho Chi Minh City</button>
-//         </div>
-//         <div>
-//             <button>Vientiane</button>
-//             <button>Sai Gon</button>
-//         </div>
-//     </section>`;
-//     document.body.innerHTML = html;
-// }
+
+function showQuestion(){
+    title.innerText = questionsAndAnswers[1][0];
+    let firstAnswer = document.createElement('button');
+    let secondAnswer = document.createElement('button');
+    let thirdAnswer = document.createElement('button');
+    let fourthAnswer = document.createElement('button');
+
+    firstAnswer.innerText = '1993';
+    secondAnswer.innerText = '1994';
+    thirdAnswer.innerText = '1991';
+    fourthAnswer.innerText = '1992';
+
+    
+}
